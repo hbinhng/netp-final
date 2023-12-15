608c472a1bc518ba083f8d0661a9086088479713
hbinhng <binhmattroi@gmail.com>
2023-12-15 12:41:16 +0000

    Add test, fix indent

--- applications/org.uet.int3304.gateway/src/main/java/org/uet/int3304/gateway/Program.java
+++ applications/org.uet.int3304.gateway/src/main/java/org/uet/int3304/gateway/Program.java
@@ -1,7 +1,7 @@
 package org.uet.int3304.gateway;
 
 public class Program {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
---
+++ applications/org.uet.int3304.gateway/src/test/java/org/uet/int3304/gateway/ProgramTest.java
@@ -0,0 +1,18 @@
+package org.uet.int3304.gateway;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ProgramTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
