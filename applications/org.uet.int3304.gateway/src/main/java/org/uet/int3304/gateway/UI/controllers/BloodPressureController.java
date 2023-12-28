029ed8de7f7964e8bb8e534f6e4b8ca28282c35a
hbinhng <binhmattroi@gmail.com>
2023-12-28 10:22:44 +0000

    Implement abstract ChartController

--- applications/org.uet.int3304.gateway/src/main/java/org/uet/int3304/gateway/UI/controllers/BloodPressureController.java
+++ applications/org.uet.int3304.gateway/src/main/java/org/uet/int3304/gateway/UI/controllers/BloodPressureController.java
@@ -1,28 +1,23 @@
 package org.uet.int3304.gateway.UI.controllers;
 
-import java.util.LinkedList;
-
 import org.uet.int3304.gateway.bucket.Bucket;
 
-import javafx.collections.FXCollections;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 
-public class BloodPressureController {
+public class BloodPressureController extends ChartController {
   private static final long DATA_RETENTION = 30 * 1000; // 30 seconds;
-
-  private final XYChart.Series<Number, Number> systolicSeries;
-  private final XYChart.Series<Number, Number> diastolicSeries;
 
   private final Bucket systolicBucket;
   private final Bucket diastolicBucket;
 
   public BloodPressureController(LineChart<Number, Number> chart) {
+    super();
     systolicBucket = new Bucket(DATA_RETENTION);
     diastolicBucket = new Bucket(DATA_RETENTION);
 
-    systolicSeries = new XYChart.Series<>();
-    diastolicSeries = new XYChart.Series<>();
+    var systolicSeries = new XYChart.Series<Number, Number>();
+    var diastolicSeries = new XYChart.Series<Number, Number>();
 
     var chartData = chart.getData();
     chartData.add(systolicSeries);
@@ -32,32 +27,13 @@ public class BloodPressureController {
     diastolicSeries.setName("Diastolic");
 
     chart.setLegendVisible(true);
-  }
 
-  private void syncBucket(XYChart.Series<Number, Number> series, Bucket bucket) {
-    var bucketData = bucket.getData();
-    var seriesData = new LinkedList<XYChart.Data<Number, Number>>();
-
-    var now = System.currentTimeMillis() / 1000;
-
-    for (var data : bucketData) {
-      var x = data.getKey() / 1000 - now;
-      var y = data.getValue();
-
-      seriesData.add(
-          new XYChart.Data<Number, Number>(x, y));
-    }
-
-    series.setData(FXCollections.observableList(seriesData));
+    linkSeriesWithBucket(systolicSeries, systolicBucket);
+    linkSeriesWithBucket(diastolicSeries, diastolicBucket);
   }
 
   public void pushData(double systolicValue, double diastolicValue) {
     systolicBucket.pushData(systolicValue);
     diastolicBucket.pushData(diastolicValue);
-  }
-
-  public void updateChart() {
-    syncBucket(systolicSeries, systolicBucket);
-    syncBucket(diastolicSeries, diastolicBucket);
   }
 }
