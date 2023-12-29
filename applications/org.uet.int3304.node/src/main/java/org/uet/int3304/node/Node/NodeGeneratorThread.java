9ce0a2cd0e6705d0b3ddc67536d598668869941b
hbinhng <binhmattroi@gmail.com>
2023-12-29 04:47:19 +0000

    Implement node generator thread

--- applications/org.uet.int3304.node/src/main/java/org/uet/int3304/node/Node/NodeGeneratorThread.java
+++ applications/org.uet.int3304.node/src/main/java/org/uet/int3304/node/Node/NodeGeneratorThread.java
@@ -3,10 +3,33 @@ package org.uet.int3304.node.Node;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class NodeGeneratorThread implements Runnable {
+  private class DataTransmitTask extends TimerTask {
+    private final DataOutputStream egress;
+    private final float data;
+
+    public DataTransmitTask(float data, DataOutputStream egress) {
+      this.data = data;
+      this.egress = egress;
+    }
+
+    @Override
+    public void run() {
+      var request = String.format("data %f\n", data);
+
+      try {
+        egress.write(request.getBytes());
+      } catch (IOException ignored) {
+      }
+    }
+  }
+
   private final Socket internal;
   private final DataOutputStream egress;
+  private final Timer timer;
 
   private final long spawn;
 
@@ -15,6 +38,7 @@ public class NodeGeneratorThread implements Runnable {
     egress = new DataOutputStream(internal.getOutputStream());
 
     spawn = System.currentTimeMillis();
+    timer = new Timer();
   }
 
   @Override
@@ -38,7 +62,8 @@ public class NodeGeneratorThread implements Runnable {
 
       var data = generator.next();
 
-      // TODO: Do something with data
+      // Delayed data transmit on other thread to prevent blocking this thread
+      timer.schedule(new DataTransmitTask(data, egress), 0);
 
       var diff = now - spawn;
       var interval = state.getDataInterval();
