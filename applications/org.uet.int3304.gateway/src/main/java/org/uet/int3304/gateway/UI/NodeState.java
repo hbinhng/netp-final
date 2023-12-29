5d1ae3656ce0ba0149444d43961a68d44bd12aa5
hbinhng <binhmattroi@gmail.com>
2023-12-29 08:45:37 +0000

    Make node state holds data interval only

--- applications/org.uet.int3304.gateway/src/main/java/org/uet/int3304/gateway/UI/NodeState.java
+++ applications/org.uet.int3304.gateway/src/main/java/org/uet/int3304/gateway/UI/NodeState.java
@@ -1,18 +1,23 @@
 package org.uet.int3304.gateway.UI;
 
-import java.util.List;
+import org.uet.int3304.gateway.AppConfig.Config;
 
 public class NodeState {
 	private static final Object lock = new Object();
-	private static  NodeState instance;
-	private NodeList nodeList;
+	private static NodeState instance;
+
+	private long dataInterval;
 
 	public NodeState() {
-		nodeList = new NodeList(null);
+		dataInterval = Config.getInstance().getDataPollInterval();
 	}
 
-	public void registerNode(Node node) {
-		nodeList.addNode(node);
+	public long getDataInterval() {
+		return dataInterval;
+	}
+
+	public void setDataInterval(long dataInterval) {
+		this.dataInterval = dataInterval;
 	}
 
 	public static NodeState getInstance() {
@@ -23,63 +28,5 @@ public class NodeState {
 			}
 
 		return instance;
-	}
-
-	public class NodeList {
-		private List<Node> nodes;
-
-		public NodeList(List<Node> nodes) {
-			this.nodes = nodes;
-		}
-
-		public List<Node> getNodes() {
-			return nodes;
-		}
-
-		public void setNodes(List<Node> nodes) {
-			this.nodes = nodes;
-		}
-
-		public void addNode(Node node) {
-			nodes.add(node);
-		}
-
-		public void removeNode(Node node) {
-			nodes.remove(node);
-		}
-
-		public String toString() {
-			String result = "";
-			for (Node node : nodes) {
-				result += node.getName() + ": " + node.getStatus() + "\n";
-			}
-			return result;
-		}
-	}
-
-	public class Node {
-		private String name;
-		private String status;
-
-		public Node(String name, String status) {
-			this.name = name;
-			this.status = status;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getStatus() {
-			return status;
-		}
-
-		public void setStatus(String status) {
-			this.status = status;
-		}
 	}
 }
