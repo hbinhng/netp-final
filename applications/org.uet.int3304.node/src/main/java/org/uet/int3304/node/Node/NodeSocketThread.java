adcc99f0a4f29950bea6c7d5410e70b642ff1f9b
hbinhng <binhmattroi@gmail.com>
2023-12-29 22:05:27 +0700

    Implement configuration fetch and apply

--- applications/org.uet.int3304.node/src/main/java/org/uet/int3304/node/Node/NodeSocketThread.java
+++ applications/org.uet.int3304.node/src/main/java/org/uet/int3304/node/Node/NodeSocketThread.java
@@ -1,8 +1,9 @@
 package org.uet.int3304.node.Node;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Map;
 
@@ -15,22 +16,57 @@ public class NodeSocketThread implements Runnable {
       Map.entry(NodeType.BloodPressure, 2),
       Map.entry(NodeType.Heartbeat, 3));
 
-  private final Socket internal;
-
-  private final BufferedInputStream ingress;
+  private final BufferedReader ingress;
   private final DataOutputStream egress;
 
   public NodeSocketThread(Socket socket) throws IOException {
-    internal = socket;
-
-    ingress = new BufferedInputStream(socket.getInputStream());
+    ingress = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     egress = new DataOutputStream(socket.getOutputStream());
+  }
+
+  private String readContent() {
+    String content = null;
+
+    try {
+      content = ingress.readLine();
+    } catch (IOException ex) {
+      return null;
+    }
+
+    return content;
   }
 
   private void sendContent(String content) {
     try {
       egress.write(content.getBytes());
     } catch (IOException ignored) {
+    }
+  }
+
+  private void parseConfigResponse(String content) {
+    var tokens = content.split("\\s+");
+
+    if (tokens.length < 2) {
+      return;
+    }
+
+    if (!tokens[0].equals("400")) {
+      return;
+    }
+
+    for (var token : tokens) {
+      var segments = token.split("=");
+
+      if (segments.length != 2)
+        continue;
+
+      if (segments[0].equals("DATA_INT")) {
+        try {
+          var newDataInterval = Long.parseLong(segments[1]);
+          NodeState.getInstance().setDataInterval(newDataInterval);
+        } catch (NumberFormatException ignored) {
+        }
+      }
     }
   }
 
@@ -47,8 +83,27 @@ public class NodeSocketThread implements Runnable {
 
     sendContent(registerCommand);
 
+    sendContent("configure\n");
+
+    var content = readContent();
+
+    if (content == null) {
+      System.err.println("Did not receive anything from server");
+      System.err.println("Exitting");
+      System.exit(-1);
+    }
+
+    parseConfigResponse(content);
+
     while (true) {
-      // Wait for config
+      content = readContent();
+
+      if (content == null) {
+        System.out.println("Connection closed unexpectedly by remote");
+        break;
+      }
+
+      parseConfigResponse(content);
     }
   }
 }
